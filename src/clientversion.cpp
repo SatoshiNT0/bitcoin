10027feeb1a33044e8c0a6dea72166a08bff7efc
Satoshi Nakamoto <154033027+SatoshiNT0@users.noreply.github.com>
2024-01-18 11:55:50 -0400

    clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2009 Satoshi Nakamoto
 // Copyright (c) 2012-2022 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -16,7 +17,7 @@
  * for both bitcoind and bitcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Satoshi satoshinakam0t0@duck.com");
 
 
 #ifdef HAVE_BUILD_INFO
@@ -82,16 +83,16 @@ std::string CopyrightHolders(const std::string& strPrefix)
     const auto copyright_devs = strprintf(_(COPYRIGHT_HOLDERS).translated, COPYRIGHT_HOLDERS_SUBSTITUTION);
     std::string strCopyrightHolders = strPrefix + copyright_devs;
 
-    // Make sure Bitcoin Core copyright is not removed by accident
-    if (copyright_devs.find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+    // Make sure Satoshi Nakamoto satoshinakam0t0@duck.com copyright is not removed by accident
+    if (copyright_devs.find("Satoshi Nakamoto Satoshinakam0t0@duck.com") == std::string::npos) {
+        strCopyrightHolders += "\n" + strPrefix + "The Main Bitcoin developer";
     }
     return strCopyrightHolders;
 }
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/SatoshiNT0/bitcoin";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i").translated, 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
